4b257204132107061486485a7c56b7dd602426cc
Ralitsa Lekova <156001297+xralitsa@users.noreply.github.com>
2024-01-24 14:53:22 +0200

    Create Exercise1.cs

---
+++ C# Fundamentals/Lab data types/Exercise1.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Lab_Work_Data_types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
