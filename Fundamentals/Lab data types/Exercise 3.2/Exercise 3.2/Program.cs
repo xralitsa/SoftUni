aea4e64c6db455c8969f327d405e86d030df5e0d
xralitsa <156001297+xralitsa@users.noreply.github.com>
2024-01-25 14:21:55 +0200

    exercise 3 solution

---
+++ Fundamentals/Lab data types/Exercise 3.2/Exercise 3.2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercise_3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            decimal sumNumbers = 0m;
+             
+            for (int i = 1; i <= n; i++)
+            {
+                decimal newNumber = decimal.Parse(Console.ReadLine());
+                sumNumbers += newNumber;
+            }
+            Console.WriteLine(sumNumbers);
+        }
+    }
+}
