93e14f0012227e5d9830543383d265eaff168621
xralitsa <156001297+xralitsa@users.noreply.github.com>
2024-01-25 15:05:54 +0200

    exercise 5 solution

---
+++ Fundamentals/Lab data types/Exercise 5/Exercise 5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercise_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                int sumOfNDigits = 0;
+                int digits = i;
+                while (digits > 0)
+                {
+                    sumOfNDigits += digits % 10;
+                    digits = digits / 10;
+                }
+
+                bool specialSum = (sumOfNDigits == 5) || (sumOfNDigits == 7) || (sumOfNDigits == 11);
+                Console.WriteLine("{0} -> {1}", i, specialSum);
+            }
+        }
+    }
+}
