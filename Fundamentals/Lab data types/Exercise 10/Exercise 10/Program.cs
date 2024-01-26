5b6e3bb45d2199f284a62b08a4c54c5db120555f
xralitsa <156001297+xralitsa@users.noreply.github.com>
2024-01-26 13:16:34 +0200

    exercise 10 solution

---
+++ Fundamentals/Lab data types/Exercise 10/Exercise 10/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercise_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstInput = char.Parse(Console.ReadLine());
+            bool result;
+            result = Char.IsLower(firstInput);
+            if (result == true)
+            {
+                Console.WriteLine("lower-case");
+            }
+            else 
+            {
+                Console.WriteLine("upper-case");
+            }
+        }
+    }
+}
