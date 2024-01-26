e2db963bdf8b3586b2f407aa7112e69912c27858
xralitsa <156001297+xralitsa@users.noreply.github.com>
2024-01-26 12:46:42 +0200

    exercise 6 solution

---
+++ Fundamentals/Lab data types/Exercise 6/Exercise 6/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Exercise_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstLetter = char.Parse(Console.ReadLine());
+            char secondLetter = char.Parse(Console.ReadLine());
+            char thirdLetter = char.Parse(Console.ReadLine());
+            Console.WriteLine("{0} {1} {2}", thirdLetter, secondLetter, firstLetter);
+        }
+    }
+}
