40fabe19bb1960d282e206c963825ede6d61c915
xralitsa <156001297+xralitsa@users.noreply.github.com>
2024-01-26 12:52:58 +0200

    exercise 7 solution

---
+++ Fundamentals/Lab data types/Exercise 7/Exercise 7/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Exercise_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstName = Console.ReadLine();
+            string lastName = Console.ReadLine();
+            string symbol = Console.ReadLine();
+            Console.WriteLine($"{firstName}{symbol}{lastName}");
+        }
+    }
+}
