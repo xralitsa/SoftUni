8dc84dd71711787be2e611684501aff0cb97a297
xralitsa <156001297+xralitsa@users.noreply.github.com>
2024-01-26 14:34:09 +0200

    exercise 13 solution

---
+++ Fundamentals/Lab data types/Exercise 13/Exercise 13/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Exercise_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+            int forthNumber = int.Parse(Console.ReadLine());
+            Console.WriteLine(((firstNumber + secondNumber) / thirdNumber) * forthNumber);
+        }
+    }
+}
