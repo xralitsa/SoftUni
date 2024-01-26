a5b0c7551ef57119f6e9ea143b52d2d53ab64b2b
xralitsa <156001297+xralitsa@users.noreply.github.com>
2024-01-26 13:43:32 +0200

    exercise 12 solution

---
+++ Fundamentals/Lab data types/Exercise 12/Exercise 12/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exercise_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= number; i++)
+
+            {
+                int sum = 0;
+                int digit = i;
+
+                while (digit > 0)
+
+                {
+
+                    sum += digit % 10;
+
+                    digit = digit / 10;
+
+                }
+
+                bool isSpecialSum = (sum == 5) || (sum == 7) || (sum == 11);
+
+                Console.WriteLine("{0} -> {1}", i, isSpecialSum);
+            }
+        }
+    }
+}
