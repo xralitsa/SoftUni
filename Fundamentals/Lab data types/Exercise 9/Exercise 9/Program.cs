c98c78056ed5f06a55a1113db68a08aa1ab6e105
xralitsa <156001297+xralitsa@users.noreply.github.com>
2024-01-26 13:11:29 +0200

    exercise 9 solution

---
+++ Fundamentals/Lab data types/Exercise 9/Exercise 9/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Exercise_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstSymbol = char.Parse(Console.ReadLine());
+            char secondSymbol = char.Parse(Console.ReadLine());
+            char thirdSymbol = char.Parse(Console.ReadLine());
+            Console.WriteLine($"{firstSymbol}{secondSymbol}{thirdSymbol}");
+        }
+    }
+}
