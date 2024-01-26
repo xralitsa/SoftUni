4b060e8c029fa386983ff7fc66482c9f7c383c85
xralitsa <156001297+xralitsa@users.noreply.github.com>
2024-01-26 12:57:58 +0200

    exercise 8 solution

---
+++ Fundamentals/Lab data types/Exercise 8/Exercise 8/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Exercise_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string townName = Console.ReadLine();
+            int populationCount = int.Parse(Console.ReadLine());
+            int townArea = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Town {townName} has population of {populationCount} and area {townArea} square km.");
+        }
+    }
+}
