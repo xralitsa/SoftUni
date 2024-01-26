41c9e79497b0650ca084ac6bf478c46508911ef2
xralitsa <156001297+xralitsa@users.noreply.github.com>
2024-01-26 13:23:32 +0200

    Exercise 11 solution

---
+++ Fundamentals/Lab data types/Exercise 11/Exercise 11/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exercise_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Length: ");
+
+            double length = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Width: ");
+
+            double width = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Heigth: ");
+
+            double heigth = double.Parse(Console.ReadLine());
+
+            double volumePyramide = (length + width + heigth) / 3;
+
+            Console.WriteLine($"Pyramid Volume: {volumePyramide:f2}");
+        }
+    }
+}
