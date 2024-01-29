c366b46e4e2a8564b112cb74e5ba2895011ca03b
xralitsa <156001297+xralitsa@users.noreply.github.com>
2024-01-29 14:12:17 +0200

    Exercise 19 solution

---
+++ Fundamentals/Lab data types/Exercise 17/Exercise 17/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercise_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            string lineOfSymbols = null;
+
+            for (int i = firstNumber; i <= secondNumber; i++)
+            {
+                char symbol = (char)i;
+                lineOfSymbols += symbol;
+                lineOfSymbols += ' ';
+            }
+
+            Console.WriteLine(lineOfSymbols);
+        }
+    }
+}
---
+++ Fundamentals/Lab data types/Exercise 19/Exercise 19/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exercise_19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sumVolume = 0;
+
+            for(int i = 1; i <= n; i++)
+            {
+                int additionalVolume = int.Parse(Console.ReadLine());
+
+                if ((sumVolume + additionalVolume) > 255)
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                }
+
+                else 
+                {
+                    sumVolume += additionalVolume;
+                }
+            }
+
+            Console.WriteLine(sumVolume);
+        }
+    }
+}
