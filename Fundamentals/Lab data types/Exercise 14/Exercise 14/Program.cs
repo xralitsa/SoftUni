196555081b8b6cbecb3a4f8ee33ea7f0e356971f
xralitsa <156001297+xralitsa@users.noreply.github.com>
2024-01-29 12:51:36 +0200

    exercise 14 solution

---
+++ Fundamentals/Lab data types/Exercise 14/Exercise 14/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercise_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int sum = 0;
+            
+            while (number != 0)
+            {
+                int digit = number % 10;
+                sum += digit;
+                number = number / 10;
+
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
