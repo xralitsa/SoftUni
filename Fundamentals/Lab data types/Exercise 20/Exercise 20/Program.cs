3596afdef621ac374a48064ded1b5ccf66c85964
xralitsa <156001297+xralitsa@users.noreply.github.com>
2024-01-29 21:12:55 +0200

    change

---
+++ Fundamentals/Lab data types/Exercise 20/Exercise 20/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercise_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
