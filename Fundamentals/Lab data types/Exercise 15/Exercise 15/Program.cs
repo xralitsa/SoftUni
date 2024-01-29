a5817b7e4607683fd410dfa4bcf909b6087f1ced
xralitsa <156001297+xralitsa@users.noreply.github.com>
2024-01-29 13:06:07 +0200

    Exercise 15 solution

---
+++ Fundamentals/Lab data types/Exercise 15/Exercise 15/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Exercise_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int peopleCount = int.Parse(Console.ReadLine());
+            int capacity = int.Parse(Console.ReadLine());
+           
+            //simple solution:
+            //if (peopleCount % capacity == 0)
+            //{
+            //    courses = peopleCount / capacity;
+            //}
+
+            //else 
+            //{
+            //   courses = peopleCount / capacity + 1;
+            // }
+            //second solution:
+
+            int courses = (int)Math.Ceiling((double)peopleCount / capacity);
+            Console.WriteLine(courses);
+        }
+    }
+}
