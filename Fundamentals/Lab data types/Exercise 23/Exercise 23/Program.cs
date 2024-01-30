0bab3166d21aeac51302073e52b1c1c37bd9a1f1
xralitsa <156001297+xralitsa@users.noreply.github.com>
2024-01-30 16:52:24 +0200

    Exercise 23 solution

---
+++ Fundamentals/Lab data types/Exercise 23/Exercise 23/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Exercise_23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int bestSnowballSnow = 0;
+            int bestSnowballTime = 0;
+            int bestSnowballQuality = 0;
+            long bestSnowballValue = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                 int snowballSnow = int.Parse(Console.ReadLine());
+                 int snowballTime = int.Parse(Console.ReadLine());
+                 int snowballQuality = int.Parse(Console.ReadLine());
+
+                 long newSnowballValue = (long)Math.Pow(snowballSnow / snowballTime, snowballQuality);
+
+                if (newSnowballValue > bestSnowballValue)
+                {
+                    bestSnowballValue = newSnowballValue;
+                    bestSnowballSnow = snowballSnow;
+                    bestSnowballTime = snowballTime;
+                    bestSnowballQuality = snowballQuality;
+
+                }
+            }
+
+            Console.WriteLine($"{bestSnowballSnow} : {bestSnowballTime} = {bestSnowballValue} ({bestSnowballQuality})");
+        }
+    }
+}
