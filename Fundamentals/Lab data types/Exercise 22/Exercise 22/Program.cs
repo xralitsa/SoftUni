82589062de72bdea6f37e2058ee1ff75d8a25525
xralitsa <156001297+xralitsa@users.noreply.github.com>
2024-01-30 16:02:42 +0200

    Exercise 22 solution

---
+++ Fundamentals/Lab data types/Exercise 22/Exercise 22/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exercise_22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            int startValue = n;
+            int y = int.Parse(Console.ReadLine());
+            int targets = 0;
+
+            while (n >= m)
+            {
+
+                n -= m;
+                targets++;
+
+                if (n == (int)Math.Ceiling((double)startValue * 50 / 100) && y != 0)
+                {
+                    n /= y;
+                }
+
+            }
+
+            Console.WriteLine(n);
+            Console.WriteLine(targets);
+
+        }
+    }
+}
