da6c86fee3acc09f78ad47e35ab75bc73a9ad460
xralitsa <156001297+xralitsa@users.noreply.github.com>
2024-01-30 12:29:54 +0200

    Exercise 20 solution

--- Fundamentals/Lab data types/Exercise 20/Exercise 20/Program.cs
+++ Fundamentals/Lab data types/Exercise 20/Exercise 20/Program.cs
@@ -6,7 +6,27 @@ namespace Exercise_20
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int kegsQuantity = int.Parse(Console.ReadLine());
+            double maxVolume = 0;
+            string kegMaxVolume = null;
+
+            for (int i = 1; i <= kegsQuantity; i++)
+            {
+                string model = Console.ReadLine();
+                double radius = double.Parse(Console.ReadLine());
+                int height = int.Parse(Console.ReadLine());
+
+                double volume = Math.PI * radius * radius * (double)height;
+
+                if (volume > maxVolume)
+                {
+                    maxVolume = volume;
+                    kegMaxVolume = model;
+                }
+
+            }
+
+            Console.WriteLine(kegMaxVolume);
         }
     }
 }
