7dc2b208a2ca45c3841fc877685039b1f38a8805
xralitsa <156001297+xralitsa@users.noreply.github.com>
2024-01-30 13:17:10 +0200

    Exercise 21 solution

---
+++ Fundamentals/Lab data types/Exercise 21/Exercise 21/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercise_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int spiceProductionPerDay = int.Parse(Console.ReadLine());
+            int spiceReserve = 0;
+            int workingDays = 0;
+            
+            while (spiceProductionPerDay >= 100)
+            {
+                spiceReserve = spiceReserve + spiceProductionPerDay - 26;
+                workingDays++;
+                spiceProductionPerDay -= 10;
+            }
+
+            spiceReserve -= 26;
+            Console.WriteLine($"{workingDays}\n{spiceReserve}");
+
+        }
+    }
+}
