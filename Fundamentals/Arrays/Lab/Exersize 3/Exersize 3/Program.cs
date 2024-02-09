ee64f1ec680cd9119571165fb8ee5b420f06ab51
xralitsa <156001297+xralitsa@users.noreply.github.com>
2024-02-09 13:42:56 +0200

    lab 3 solution

---
+++ Fundamentals/Arrays/Lab/Exersize 3/Exersize 3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace Exersize_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            double[] arr = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i] + " => " + (int)Math.Round(arr[i],MidpointRounding.AwayFromZero));
+            }
+        }
+    }
+}
